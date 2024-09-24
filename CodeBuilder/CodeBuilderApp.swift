f75edd7654f097bedc533a4ada2b97cecde37e5f
mirogohacki <mgohacki@uvm.edu>
2024-09-24 08:15:49 -0400

    Initial Commit

---
+++ CodeBuilder/CodeBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBuilderApp.swift
+//  CodeBuilder
+//
+//  Created by Miro Gohacki on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBuilder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeBuilder
+//
+//  Created by Miro Gohacki on 9/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

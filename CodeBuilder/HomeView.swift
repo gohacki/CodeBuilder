6e18d45475bf434abbce39dd0149d4a47eb1c65d
aaronperkel <aaron.perkel27@gmail.com>
2024-09-24 10:33:33 -0400

    added nav bar

--- CodeBuilder/HomeView.swift
+++ CodeBuilder/HomeView.swift
@@ -7,51 +7,87 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        NavigationView {
-            VStack(spacing: 40) {
-                Text("Home")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                Spacer()
-                Text("Welcome to CodeBuilder")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                Spacer()
-                NavigationLink(destination: ProblemsView()) {
-                    Text("Problems")
-                        .font(.title2)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.accentColor)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                NavigationLink(destination: LearningView()) {
-                    Text("Learning")
-                        .font(.title2)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.green)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                NavigationLink(destination: ResumeView()) {
-                    Text("Resume Tips")
-                        .font(.title2)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.red)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                Spacer()
-            }
+  var body: some View {
+      
+    NavigationView {
+      // Title
+      VStack() {
+        Text("Home")
+          .font(.largeTitle)
+          .fontWeight(.bold)
+          .multilineTextAlignment(.center)
+        
+        // Title
+        Text("Welcome to CodeBuilder")
+          .font(.largeTitle)
+          .fontWeight(.bold)
+          .multilineTextAlignment(.center)
+        
+        // Blue Button
+        NavigationLink(destination: ProblemsView()) {
+          Text("Problems")
+            .font(.title2)
+            .frame(maxWidth: .infinity)
             .padding()
+            .background(Color.accentColor)
+            .foregroundColor(.white)
+            .cornerRadius(10)
         }
+        
+        // Green Button
+        NavigationLink(destination: LearningView()) {
+            Text("Learning")
+                .font(.title2)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+        
+        // Red Button
+        NavigationLink(destination: ResumeView()) {
+            Text("Resume Tips")
+                .font(.title2)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+        Spacer()
+      }
+      .padding()
     }
+    
+    // Nav Bar at Bottom
+    VStack {
+      TabView {
+        // Home
+        VStack {}
+        .background(.blue)
+        .tabItem {
+          Image(systemName: "house.fill")
+          Text("Home")
+        }
+        
+        // Search
+        VStack {}
+        .background(.gray)
+        .tabItem {
+          Image(systemName: "magnifyingglass")
+          Text("Search")
+        }
+        
+        // Profile
+        VStack {}
+        .tabItem {
+          Image(systemName: "person.fill")
+          Text("Profile")
+        }
+      }
+    }
+  }
 }
 
 #Preview{

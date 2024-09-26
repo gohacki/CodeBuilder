fb0affb54f84940bd127b560f696ecebb600de7b
aaronperkel <aaron.perkel27@gmail.com>
2024-09-26 16:02:38 -0400

    redesigning home page, it is soo laggy

--- CodeBuilder/Views/Home/HomeView.swift
+++ CodeBuilder/Views/Home/HomeView.swift
@@ -13,44 +13,35 @@ struct HomeView: View {
     NavigationStack {
       VStack {
         TabView {
-          NavigationLink(destination: ProblemsView()) {
-            VStack {
-              Image(systemName: "doc.text.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 100)
-                .foregroundStyle(.blue)
-              Text("Problems")
-                .font(.headline)
-                .foregroundStyle(Color.primary)
+          VStack {
+            Image(systemName: "doc.text.fill")
+              .resizable()
+              .scaledToFit()
+              .frame(height: 100)
+              .foregroundStyle(.blue)
+            Text("Problems")
+              .font(.headline)
+          }
+          VStack {
+            Image(systemName: "book.fill")
+              .resizable()
+              .scaledToFit()
+              .frame(height: 100)
+              .foregroundStyle(.orange)
+            Text("Learning")
+              .font(.headline)
+          }
+          VStack {
+            Image(systemName: "briefcase.fill")
+              .resizable()
+              .scaledToFit()
+              .frame(height: 100)
+              .foregroundStyle(.brown)
+            Text("Resume Tips")
+              .font(.headline)
             }
           }
-          NavigationLink(destination: LearningView()) {
-            VStack {
-              Image(systemName: "book.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 100)
-                .foregroundStyle(.orange)
-              Text("Learning")
-                .font(.headline)
-                .foregroundStyle(Color.primary)
-            }
-          }
-          
-          NavigationLink(destination: ResumeView()) {
-            VStack {
-              Image(systemName: "briefcase.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 100)
-                .foregroundStyle(.brown)
-              Text("Resume Tips")
-                .font(.headline)
-                .foregroundStyle(Color.primary)
-            }
-          }
-        }
+        .padding(.bottom, 20)
         .tabViewStyle(PageTabViewStyle())
         .frame(height: 200)
       }
@@ -59,12 +50,34 @@ struct HomeView: View {
         .font(.headline)
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.leading, 15)
-      
-      
+  
       List {
+        NavigationLink(destination: ProblemsView()) {
+          HStack {
+            Image(systemName: "doc.text.fill")
+              .foregroundColor(.blue)
+            Text("Problems")
+          }
+        }
+        NavigationLink(destination: LearningView()) {
+          HStack {
+            Image(systemName: "book.fill")
+              .foregroundColor(.blue)
+            Text("Learning")
+          }
+        }
+                
+        NavigationLink(destination: ResumeView()) {
+          HStack {
+            Image(systemName: "briefcase.fill")
+              .foregroundColor(.blue)
+            Text("Resume Tips")
+          }
+        }
         Text("Maybe here we can have like")
         Text("stats or some other useful info")
       }
+      
       .navigationTitle("Home")
       .navigationBarTitleDisplayMode(.large)
     }
